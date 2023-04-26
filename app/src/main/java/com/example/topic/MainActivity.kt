ddd5af0529402b7c30af6e5d7edf9c2544fbd37a
alex <alex.vo@cubepayment.com>
2023-04-26 23:34:04 +0800

    Finished version 1

--- app/src/main/java/com/example/topic/MainActivity.kt
+++ app/src/main/java/com/example/topic/MainActivity.kt
@@ -4,26 +4,37 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.courses.R
+import com.example.courses.R.*
 import com.example.topic.data.DataSource
 import com.example.topic.model.Topic
 import com.example.topic.ui.theme.TopicTheme
@@ -39,9 +50,25 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    TopicCardList(DataSource.topics)
+                    TopicGrid(
+                        modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_small))
+                    )
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun TopicGrid(modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small)),
+        horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small)),
+        modifier = modifier
+    ) {
+        items(DataSource.topics) { topic ->
+            TopicCard(topic)
         }
     }
 }
@@ -50,28 +77,88 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TopicCardList(topicList: List<Topic>) {
     LazyColumn {
-        items(topicList) {item ->
-            TopicCard(topic = item)
+        items(topicList) { item ->
+            Row {
+                TopicCard(topic = item)
+            }
         }
     }
 }
 
 @Composable
 fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
-    Card(modifier = Modifier.padding(8.dp).fillMaxWidth()) {
+    Card {
+        Row {
+            Box {
+                Image(
+                    painter = painterResource(id = topic.imageResourceId),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(width = 68.dp, height = 68.dp)
+                        .aspectRatio(1f),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Column {
+                Text(
+                    text = stringResource(id = topic.nameRes),
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(
+                        start = dimensionResource(id = R.dimen.padding_medium),
+                        top = dimensionResource(id = R.dimen.padding_medium),
+                        end = dimensionResource(id = R.dimen.padding_medium),
+                        bottom = dimensionResource(id = R.dimen.padding_small)
+                    )
+                )
+
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_grain),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(start = dimensionResource(id = R.dimen.padding_medium))
+                    )
+                    Text(
+                        text = topic.availableCourses.toString(),
+                        style = MaterialTheme.typography.labelMedium,
+                        modifier = Modifier.padding(start = dimensionResource(id = R.dimen.padding_small))
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun TopicCard1(topic: Topic, modifier: Modifier = Modifier) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+    ) {
         Row(modifier = Modifier.wrapContentWidth()) {
             Image(
                 painter = painterResource(id = topic.imageResourceId),
-                contentDescription = stringResource(id = topic.stringResourceId),
+                contentDescription = stringResource(id = topic.nameRes),
                 //modifier = Modifier.fillMaxHeight()
             )
             Column {
-                Text(text = stringResource(id = topic.stringResourceId),
+                Text(
+                    text = stringResource(id = topic.nameRes),
                     modifier = Modifier.padding(8.dp)
                 )
-                Text(text = "${topic.numberViews}",
-                    modifier = Modifier.padding(8.dp)
-                )
+                Row {
+                    Icon(
+                        painter = painterResource(id = drawable.ic_grain),
+                        contentDescription = null,
+                        modifier = Modifier.padding(8.dp)
+                    )
+                    Text(
+                        text = "${topic.availableCourses}",
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
             }
         }
     }
@@ -81,6 +168,6 @@ fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
 @Preview
 fun TopicCardPreview() {
     TopicTheme {
-        TopicCard(Topic(R.string.architecture, 152, R.drawable.architecture))
+        TopicCard(Topic(string.architecture, 152, drawable.architecture))
     }
 }
--- app/src/main/java/com/example/topic/model/Topic.kt
+++ app/src/main/java/com/example/topic/model/Topic.kt
@@ -3,4 +3,4 @@ package com.example.topic.model
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 
-data class Topic(@StringRes val stringResourceId: Int, val numberViews: Int, @DrawableRes val imageResourceId: Int)
+data class Topic(@StringRes val nameRes: Int, val availableCourses: Int, @DrawableRes val imageResourceId: Int)
